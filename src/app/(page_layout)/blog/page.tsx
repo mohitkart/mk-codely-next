ef2831f9ddeb2e72f1a82965dd72269a4eed62e2
Mohit Kumar <mohitkartweb@gmail.com>
2025-09-20 16:01:18 +0530

    ccode merhed

--- src/app/(page_layout)/blog/page.tsx
+++ src/app/(page_layout)/blog/page.tsx
@@ -1,16 +1,15 @@
 import { Metadata } from "next";
 import { APP_DESCRIPTION, APP_NAME } from "@/utils/shared";
-import MenuPage from "./MenuPage";
 
 export const metadata: Metadata = {
-  title: `${APP_NAME} - Menu`,
+  title: `${APP_NAME} - Blogs`,
   description: APP_DESCRIPTION,
 };
 
 export default function Home() {
   return (
    <>
-   <MenuPage/>
+  blogs
    </>
   );
 }
